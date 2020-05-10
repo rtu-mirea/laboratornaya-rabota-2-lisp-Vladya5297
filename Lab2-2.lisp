e2c581abc024a7187728a5cd30d8fba9c4079387
Vladya5297 <43731115+Vladya5297@users.noreply.github.com>
2020-05-10 17:44:43 +0300

    Delete Lab2-2.lisp

--- Lab2-2.lisp
+++
@@ -1,5 +0,0 @@
-(let ((in (open "./text.txt" :if-does-not-exist nil)))
-               (when in
-                 (loop for line = (read-line in nil)
-                      while line do (format t "~a~%" line))
-                 (close in)))
